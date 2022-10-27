4efe386e62de197bdb72b8cfa2696be84c9fb886
Emanuel Borges <ebbmango@tuta.io>
2022-10-27 18:54:35 -0300

    built bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,28 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save!
+      redirect_to list_path(@bookmark.list_id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    list_id = @bookmark.list_id
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(list_id), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
 end
