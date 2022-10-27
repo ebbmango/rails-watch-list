3872f23586ff353cd2dd5959d7cb086b72b4e7ce
Emanuel Borges <ebbmango@tuta.io>
2022-10-27 18:41:27 -0300

    lists controller done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,38 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show; end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    if @list.save!
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.new(list_params)
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
