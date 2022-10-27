bd76e23657305a8de8587a57a402b774d7bcf9d5
Emanuel Borges <ebbmango@tuta.io>
2022-10-27 16:37:24 -0300

    sets up all routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'lists#index'
+
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: :destroy
+  resources :movies, only: :index
 end
